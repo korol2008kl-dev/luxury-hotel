165c1df6206330d671d6aa6a3198a9c77865f85c
korol2008kl-dev <korol2008kl@gmail.com>
2026-04-15 10:18:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function goBooking() {
+    window.location.href = "booking.html";
+}     
+
+function openMenu() {
+    document.getElementById("sidebar").style.left = "0";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.left = "-250px";
+    document.getElementById("overlay").style.display = "none";
+}
